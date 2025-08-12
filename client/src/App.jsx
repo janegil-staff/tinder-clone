a5581b9ecbc627591235e40dd38abe917463bb44
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-08-12 12:38:11 +0200

    Add vite client with tailwindcss

---
+++ client/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div className="text-red-500">Home</div>;
+}
+
+export default App;
---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust based on your file types
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
