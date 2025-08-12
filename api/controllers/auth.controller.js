785a616968a6e6b5dab58200bd11c9c942d06b4d
Jan Egil Staff Øvretveit <janstovr@Mac.lan>
2025-08-12 11:44:34 +0200

    Add login & logout on the api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,11 +2,10 @@ import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
 const signToken = (id) => {
-	return jwt.sign({ id }, process.env.JWT_SECRET, {
-		expiresIn: "7d",
-	});
+  return jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 };
-
 
 export const signup = async (req, res) => {
   const { name, email, password, age, gender, genderPreference } = req.body;
@@ -58,4 +57,47 @@ export const signup = async (req, res) => {
     console.log("Error in signup controller:", error);
     res.status(500).json({ success: false, message: "Server error" });
   }
+};
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "All fields are required",
+      });
+    }
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user || !(await user.matchPassword(password))) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password",
+      });
+    }
+
+    const token = signToken(user._id);
+
+    res.cookie("jwt", token, {
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in milliseconds
+      httpOnly: true, // prevents XSS attacks
+      sameSite: "strict", // prevents CSRF attacks
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    res.status(200).json({
+      success: true,
+      user,
+    });
+  } catch (error) {
+    console.log("Error in login controller:", error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+
+export const logout = async (req, res) => {
+  res.clearCookie("jwt");
+  res.status(200).json({ success: true, message: "Logged out successfully" });
 };
