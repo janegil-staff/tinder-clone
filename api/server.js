c227952040cf1ebd0701cd90a212d538d2c275f2
Jan Egil Staff Øvretveit <janstovr@gmail.com>
2025-08-12 17:17:20 +0200

    Make Ready for Deployment

--- api/server.js
+++ api/server.js
@@ -1,14 +1,24 @@
-
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 import { createServer } from "http";
 import { initializeSocket } from "./socket/socket.server.js";
+import path from "path";
 
 const httpServer = createServer(app);
 
 initializeSocket(httpServer);
 
 const PORT = process.env.PORT || 5001;
+
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/client/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+  });
+}
 
 httpServer.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
